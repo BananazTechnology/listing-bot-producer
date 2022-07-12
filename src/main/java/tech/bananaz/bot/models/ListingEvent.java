c8ca356ae1ea2ee28b32dddfbfb4a5c57f55ef7b
Aaron Renner <arennerwv@gmail.com>
2022-07-12 14:56:22 -0400

    2.3.0 Added size restriction on tokenId

--- src/main/java/tech/bananaz/bot/models/ListingEvent.java
+++ src/main/java/tech/bananaz/bot/models/ListingEvent.java
@@ -56,6 +56,7 @@ public class ListingEvent implements Comparable<ListingEvent> {
 	private Instant    createdDate;
 	private Instant    startTime;
 	private Instant    endTime;
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String     tokenId;
 	@Column(columnDefinition = "VARCHAR(75)")
 	private String     collectionName;
